63e6451c982ecf4027f48cc090dfef1aa0266ea8
Circle Lin <nilcric@outlook.com>
2021-10-14 15:34:30 +0800

    mv server

--- src/sekai.js
+++ src/sekai.js
@@ -1,25 +1,31 @@
 import axios from 'axios';
 import JSONbig from 'json-bigint';
 
-const post = urls => axios.post('https://mo.zju.edu.cn/pyapi/apps/run/5fc78a949a6edafe98c1f3a7', {
-  'app': {
-    'input': {
-      'urls': {
-        'val': urls,
-        'type': '[str]'
-      }
-    },
-    'output': {}
-  },
-  'version': 'dev'
-}, {
-  transformResponse: data => JSONbig({ storeAsString: true }).parse(data),
-});
-
 export default function (url) {
-  if (url instanceof Array) {
-    return post(url).then(response => response.data.response.responses);
-  } else {
-    return post([url]).then(response => response.data.response.responses[0]);
-  }
+  return axios.get('https://api.pjsekai.moe' + url, {
+    transformResponse: data => JSONbig({ storeAsString: true }).parse(data),
+  }).then(response => response.data);
 }
+
+// const post = urls => axios.post('https://mo.zju.edu.cn/pyapi/apps/run/5fc78a949a6edafe98c1f3a7', {
+//   'app': {
+//     'input': {
+//       'urls': {
+//         'val': urls,
+//         'type': '[str]'
+//       }
+//     },
+//     'output': {}
+//   },
+//   'version': 'dev'
+// }, {
+//   transformResponse: data => JSONbig({ storeAsString: true }).parse(data),
+// });
+
+// export default function (url) {
+//   if (url instanceof Array) {
+//     return post(url).then(response => response.data.response.responses);
+//   } else {
+//     return post([url]).then(response => response.data.response.responses[0]);
+//   }
+// }
